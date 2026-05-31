bd1d0b89a90b9ee6280978126a5f38d399790629
Amirreza <ar.ghanavatinezhad@dadehpardaz.com>
2026-05-31 16:42:50 +0330

    composer added to project, use psr-4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fa976489c9bfa86be7f1e9916b653e7bcbf826a0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fa976489c9bfa86be7f1e9916b653e7bcbf826a0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
